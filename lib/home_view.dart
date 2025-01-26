baac8b8eae9f60bfd022df460de219eed7ff9c21
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-01-26 15:41:44 +0200

    create custom container

--- lib/home_view.dart
+++ lib/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qrcode/widgets/custom_container.dart';
 import 'package:qrcode/widgets/title_widget.dart';
 
 class HomeView extends StatelessWidget {
@@ -22,28 +23,3 @@ class HomeView extends StatelessWidget {
   }
 }
 
-class CustomContainer extends StatelessWidget {
-  const CustomContainer({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: EdgeInsets.all(50),
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.surface,
-          borderRadius: BorderRadius.circular(24),
-          boxShadow: [
-            BoxShadow(
-              spreadRadius: 5,
-              blurRadius: 10,
-              color: Colors.black12.withOpacity(0.1),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
---
+++ lib/widgets/custom_container.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: EdgeInsets.all(50),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          borderRadius: BorderRadius.circular(24),
+          boxShadow: [
+            BoxShadow(
+              spreadRadius: 5,
+              blurRadius: 10,
+              color: Colors.black12.withOpacity(0.1),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
