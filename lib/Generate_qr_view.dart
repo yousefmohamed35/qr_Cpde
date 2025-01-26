23b1eaa8387d599adc6c7b8b9da4041cb123b652
yousefmohamed35 <yousefmo2021jo@gmail.com>
2025-01-26 18:04:24 +0200

    create generate qr code view body

--- lib/Generate_qr_view.dart
+++ lib/Generate_qr_view.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:qrcode/functions/data.dart';
+import 'package:qrcode/widgets/generate_qr_view_body.dart';
 import 'package:qrcode/widgets/select_button_widget.dart';
 import 'package:screenshot/screenshot.dart';
 import 'package:share_plus/share_plus.dart';
@@ -62,38 +63,14 @@ class _GenerateQrViewState extends State<GenerateQrView> {
       child: Scaffold(
         backgroundColor: Colors.deepPurple,
         appBar: buildAppBar(),
-        body: Container(
-          padding: EdgeInsets.all(16),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Card(
-                  color: Colors.white,
-                  elevation: 0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(16),
-                  ),
-                  child: Padding(
-                    padding: EdgeInsets.all(8),
-                    child: Column(
-                      children: [
-                        SelectButtonWidget(
-                          selectedType: selectedType,
-                          onSelectionChanged: (Set<String> selection) {
-                            setState(() {
-                              selectedType = selection.first;
-                              qrData = '';
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+        body: GenerateQrViewBody(
+          selectedType: selectedType,
+          onSelectionChanged: (Set<String> selection) {
+            setState(() {
+              selectedType = selection.first;
+              qrData = '';
+            });
+          },
         ),
       ),
     );
@@ -140,4 +117,3 @@ class _GenerateQrViewState extends State<GenerateQrView> {
     }
   }
 }
-
---
+++ lib/widgets/generate_qr_view_body.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:qrcode/widgets/select_button_widget.dart';
+
+class GenerateQrViewBody extends StatelessWidget {
+  const GenerateQrViewBody({
+    super.key,
+    required this.selectedType, required this.onSelectionChanged,
+  });
+
+  final String selectedType;
+   final void Function(Set<String>) onSelectionChanged;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(16),
+      child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Card(
+              color: Colors.white,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(16),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(8),
+                child: Column(
+                  children: [
+                    SelectButtonWidget(
+                      selectedType: selectedType,
+                      onSelectionChanged: onSelectionChanged,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
